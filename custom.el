53e9be0d69255dba0484b0313112454c37a0ddaf
吾辈长歌 <79568068+JanSky520@users.noreply.github.com>
2023-12-07 21:27:48 +0800

    Create custom.el

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(emms all-the-icons-dired nil eglot restart-emacs all-the-icons all-the-icons-mode-line doom-modeline pdf-tools quickrun consult which-key marginalia doom-themes gnu-elpa-keyring-update orderless vertico company)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "JetBrainsMonoNL Nerd Font Mono" :height 100)))))

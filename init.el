06775479e8aeb4bd9c5e3c6cff24b9ac70632aea
吾辈长歌 <79568068+JanSky520@users.noreply.github.com>
2023-12-07 21:31:05 +0800

    Create init.el

---
+++ init.el
@@ -0,0 +1,11 @@
+;; -*- lexical-binding: t -*-
+
+(setq custom-file (expand-file-name "~/.emacs.d/custom.el"))
+(load custom-file 'no-error 'no-message)
+
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+
+(require 'basic)
+(require 'plugin)
+
+(setq file-name-coding-system 'chinese-gb18030)

a3231a3c73cf234e7faf9da4b0266265faa16507
吾辈长歌 <79568068+JanSky520@users.noreply.github.com>
2024-06-08 16:23:27 +0800

    Update plugin.el

--- lisp/plugin.el
+++ lisp/plugin.el
@@ -73,16 +73,28 @@
   :config (setq recentf-max-menu-items 10))
 
 ;;设置补全
-(use-package company
+(use-package corfu
   :ensure t
   :init
-  (global-company-mode t)
-  :config
-  (setq company-minimum-prefix-length 1)
-  (setq company-idle-delay 0))
-(use-package company-box
+  (progn
+    (setq corfu-auto t)
+    (setq corfu-cycle t)
+    (setq corfu-quit-at-boundary t)
+    (setq corfu-quit-no-match t)
+    (setq corfu-preview-current t)
+    (setq corfu-min-width 40)
+    (setq corfu-max-width 50)
+    (setq corfu-auto-delay 0.2)
+    (setq corfu-auto-prefix 1)
+    (setq corfu-on-exact-match nil)
+    (setq corfu-preselect 'promt)
+    (global-corfu-mode)
+    (corfu-popupinfo-mode)
+    ))
+(use-package nerd-icons-corfu
   :ensure t
-  :hook (company-mode . company-box-mode))
+  :config
+  (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
 
 ;;翻译插件
 (use-package sdcv

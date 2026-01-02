08e86b7bf0a85c0ff78fa88c134ccf99012eafb6
jansky520 <1102812928@qq.com>
2026-01-03 00:51:49 +0800

    更新

--- early-init.el
+++ early-init.el
@@ -1,2 +1,4 @@
+(setq package-native-compile t)
+(setq native-comp-deferred-compilation t)
 (setq gc-cons-threshold most-positive-fixnum)
 (add-hook 'after-init-hook #'(lambda () (setq gc-cons-threshold 800000)))
